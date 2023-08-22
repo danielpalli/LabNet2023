102a3adc83822bdf7c55b5ac16d9ea20803f1f5e
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2023-08-21 23:16:59 -0300

    Update punto Uno

---
+++ labNetPractica2.Actividad2/labNetPractica2.Actividad2/GenerarExepcion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace labNetPractica2.Actividad2
+{
+    public static class PuntoUno
+    {
+        public static double GenerarExcepcion(this double denominador, double valor)
+        {
+            if (denominador == 0) throw new DivideByZeroException();
+            return valor / denominador;
+        }
+    }
+}
--- labNetPractica2.Actividad2/labNetPractica2.Actividad2/Program.cs
+++ labNetPractica2.Actividad2/labNetPractica2.Actividad2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace labNetPractica2.Actividad2
 {
@@ -11,16 +7,61 @@ namespace labNetPractica2.Actividad2
         static void Main(string[] args)
         {
             PuntoUno();
+            //PuntoDos();
         }
 
-        public static void PuntoUno()
+        public static void PuntoDos()
         {
-            Console.WriteLine("Ingrese un valor:");
-            double valor = double.Parse(Console.ReadLine());
-
             try
             {
-                GenerarExcepcion(valor);
+                Console.Write("Ingrese el numerador:");
+                double dividendo = double.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el denominador:");
+                double divisor = double.Parse(Console.ReadLine());
+
+                double resultado = Program.Division(dividendo, divisor);
+                Console.WriteLine(resultado);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("¡Seguro ingresó una letra o no ingresó nada!");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("¡Solo Chuck Norris divide por cero!");
+            }
+        }
+
+
+        public static double Division(double a, double b)
+        {
+            try
+            {
+                if (b == 0)
+                {
+                    throw new DivideByZeroException("¡Solo Chuck Norris divide por cero!");
+                }
+
+                return a / b;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                return -1; //La unica forma que no me tire error. Sino me marcaba error. Hay alguna forma de resolver esto sin retornar -1?
+            }
+        }
+       
+        public static void PuntoUno()
+        {
+            try {
+
+                double denominador = 0;
+                Console.WriteLine("Ingrese un valor:");
+                double valor = double.Parse(Console.ReadLine());
+                double result = denominador.GenerarExcepcion(valor);
+
                 Console.WriteLine("Operacion exitosa");
             }
             catch (DivideByZeroException e)
@@ -32,15 +73,6 @@ namespace labNetPractica2.Actividad2
                 Console.WriteLine("Si se muestra esto, termino de realizarse la operacion");
             }
         }
-        public static void GenerarExcepcion(double valor)
-        {
-            double denominador = 0;
 
-            if (denominador == 0) throw new DivideByZeroException();
-
-            double resultado = valor / denominador;
-
-            Console.WriteLine(resultado);
-        }
     }
 }
