0942d257cb80b8eecffa589b1e374af110e4eaa9
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2023-09-01 22:30:13 -0300

    Update

--- labNetPractica3/labNetPractica3/Program.cs
+++ labNetPractica3/labNetPractica3/Program.cs
@@ -12,34 +12,59 @@ namespace labNetPractica3
     {
         static void Main(string[] args)
         {
-            //OrdersLogic ordersLogic = new OrdersLogic();
+            OrdersLogic ordersLogic = new OrdersLogic();
             CustomersLogic customersLogic = new CustomersLogic();
 
-            //Console.WriteLine("-- Customers Address:");
-
-            //foreach (Customers customer in customersLogic.GetAll())
-            //{
-            //    Console.WriteLine(customer.Address);
-            //}
-            //Console.ReadLine();
-
-            bool active = false;
+            bool exit = false;
             List<string> options = new List<string>() { "Ver clientes", "Ver ordenes", "Insertar cliente", "Eliminar cliente", "Actualizar datos cliente", "salir" };
 
-            while (!active)
+            while (!exit)
             {
                 for (int i = 0; i < options.Count ; i++)
                 {
                     Console.WriteLine($"{i + 1} {options[i]}");
                 }
-                Console.WriteLine("Probando");
-                //int optionMenu = int.Parse(Console.ReadLine());
-                //Console.WriteLine("Probando");
 
-                active = true;
+                Console.Write("Ingresa un número (6 para salir): ");
+                string option = Console.ReadLine();
 
+                switch (option)
+                {
+                    case "1":
+                        GetCustomerList(customersLogic);
+                        break;
+                    case "2":
+                        //GetOrdersList(ordersLogic);
+                        break;
+                    case"3":
+                        //AddNewCustomer(customersLogic);
+                        break;
+                    case "4":
+                        //DeleteCustomer(customersLogic);
+                        break;
+                    case "5":
+                        //UpdateCustomer(customersLogic);
+                        break;
+                    case "6":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida");
+                        break;
+                }
             }
             Console.ReadKey();
+        }
+
+        public static void GetCustomerList(CustomersLogic customersLogic)
+        {
+            Console.WriteLine("-- Customers Address:");
+
+            foreach (Customers customer in customersLogic.GetAll())
+            {
+                Console.WriteLine(customer.Address);
+            }
+            Console.ReadLine();
         }
     }
 }
