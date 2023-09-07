031bfaf65287981e49524a4332b08f14b9df4c21
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2023-09-07 18:26:30 -0300

    Update

--- practica5/main.js
+++ practica5/main.js
@@ -1,9 +1,8 @@
 import "./style.css";
 
-
 document.querySelector("#app").innerHTML = `
   <div class="container mx-auto">
-    <button class="bg-red-500 p-2 m-6 rounded-sm shadow-md text-white">
+    <button id="reset" type="button" class="bg-red-500 p-2 m-6 rounded-sm shadow-md text-white">
       Reiniciar!
     </button>
     <h1 class="font-bold text-center text-3xl">Adivinar el Número! <spam class="bg-cyan-400 rounded-3xl shadow-md px-4 py-1">(entre 1 y 20)</spam></h1>
@@ -11,15 +10,15 @@ document.querySelector("#app").innerHTML = `
 
     <div class="flex w-full h-[calc(100vh-500px)]">
 
-      <form class="w-1/2">
+      <div class="w-1/2">
         <div class="flex flex-col justify-center items-center h-[100%]">
-          <input type="number" class="border border-gray-700 rounded-sm shadow-sm p-4 text-center" placeholder="Ingrese un numero" />
-          <button class="bg-green-600 rounded-sm p-2 text-white mt-8">Probar!</button>
+          <input id="value" type="number" class="border border-gray-700 rounded-sm shadow-sm p-4 text-center" placeholder="Ingrese un numero" />
+          <button id="checkButton" type="button" class="bg-green-600 rounded-sm p-2 text-white mt-8">Probar!</button>
         </div>
-      </form>
+      </div>
 
       <div class="w-1/2 py-5">
-          <p class="my-4">result</p>
+          <p class="my-4" id="result">?</p>
           <hr>
           <div class="flex my-5">
             <p class="w-1/2">Score:</p>
@@ -31,3 +30,27 @@ document.querySelector("#app").innerHTML = `
     </div>
   </div>
 `;
+
+const btnCheck = document.getElementById("checkButton");
+const inputValue = document.getElementById("value");
+let numberToGuess = Math.floor(Math.random() * 20) + 1;
+const result = document.getElementById("result");
+const reset = document.getElementById("reset");
+
+const enviarResultado = () => {
+  if (inputValue.value == ""){
+    alert("Ingrese un numero");
+  }
+  console.log(numberToGuess);
+  if (inputValue.value == numberToGuess){
+    result.innerHTML = "Correcto!";
+  }
+}
+
+btnCheck.addEventListener("click", enviarResultado);
+reset.addEventListener("click", () => {
+  numberToGuess = Math.floor(Math.random() * 20) + 1;
+  inputValue.value = "";
+  result.innerHTML = "?";
+});
+
