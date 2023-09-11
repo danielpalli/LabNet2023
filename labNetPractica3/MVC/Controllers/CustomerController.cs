2b416cc39159d24aec5f8a7405c39547c9a8b031
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2023-09-10 23:43:14 -0300

    Update

---
+++ labNetPractica3/MVC/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using Logic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var logic = new CustomersLogic();
+            List<Customers> customers = logic.GetAll();
+            return View(customers);
+        }
+    }
+}
