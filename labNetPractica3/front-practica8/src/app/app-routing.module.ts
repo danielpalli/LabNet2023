bcef152c03153f61bf87c08b2b87145671532118
Daniel Palli <56980051+danielpalli@users.noreply.github.com>
2023-09-17 00:30:59 -0300

    Agregado rutas autenticacion y dashboard

--- labNetPractica3/front-practica8/src/app/app-routing.module.ts
+++ labNetPractica3/front-practica8/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  { path: '**', redirectTo: 'auth', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
