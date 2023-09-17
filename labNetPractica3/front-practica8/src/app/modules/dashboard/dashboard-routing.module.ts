816a0abade5bbf25d3288008bf3123ea83ea053e
Daniel Palli <56980051+danielpalli@users.noreply.github.com>
2023-09-17 12:23:54 -0300

    Agregado rutas y vistas base

--- labNetPractica3/front-practica8/src/app/modules/dashboard/dashboard-routing.module.ts
+++ labNetPractica3/front-practica8/src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardLayoutComponent,
+    children: [
+      { path: '', component: StartPageComponent },
+      { path: '**', redirectTo: '' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- labNetPractica3/front-practica8/src/app/modules/dashboard/dashboard.module.ts
+++ labNetPractica3/front-practica8/src/app/modules/dashboard/dashboard.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardLayoutComponent,
+    StartPageComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule
